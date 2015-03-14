48dbf6a0815817f71621685c973bbf98c4122c4b
zevpersellin <zevpersellin@gmail.com>
2015-03-14 10:01:26 -0600

    Update GlowShaderTestActivity.java

--- src/com/andengine/lights/GlowShaderTestActivity.java
+++ src/com/andengine/lights/GlowShaderTestActivity.java
@@ -11,32 +11,25 @@ import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
-import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.input.touch.TouchEvent;
-import org.andengine.opengl.shader.constants.ShaderProgramConstants;
-import org.andengine.opengl.shader.source.IShaderSource;
-import org.andengine.opengl.shader.source.StringShaderSource;
 import org.andengine.ui.IGameInterface;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.adt.color.Color;
 
 import com.andengine.lights.shader.GlowShader;
-import com.andengine.lights.shader.LightShader;
-import com.andengine.lights.shader.RandomColorRectangleShader;
-
 public class GlowShaderTestActivity extends BaseGameActivity implements IOnSceneTouchListener{
 	
-	public static final int WIDTH = 1080;
-	public static final int HEIGHT = 720;
+	public static final int CAMERA_WIDTH = 1080;
+	public static final int CAMERA_HEIGHT = 720;
 	
 	private Camera mCamera;
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		this.mCamera = new Camera(0, 0, WIDTH, HEIGHT);
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		
-		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(WIDTH, HEIGHT), this.mCamera);
+		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
 
 		return engineOptions;
 	}
@@ -48,11 +41,12 @@ public class GlowShaderTestActivity extends BaseGameActivity implements IOnScene
         	
 			
 	}
-
+	GlowShader mGlowShader;
 	@Override
 	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback)	throws IOException {
 	
-
+		mGlowShader = new GlowShader().getInstance();
+		this.getShaderProgramManager().loadShaderProgram(mGlowShader);
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 	 Scene scene;
@@ -61,37 +55,41 @@ public class GlowShaderTestActivity extends BaseGameActivity implements IOnScene
 			throws IOException
 	{       
 			this.mEngine.registerUpdateHandler(new FPSLogger());
-	        scene = new Scene();	        
-	        scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));	        
-	        getEngine().registerUpdateHandler(new FPSLogger());
+	        scene = new Scene();	        	
 	        scene.setOnSceneTouchListener(this);
+	        createGlowRectangle();
 	        pOnCreateSceneCallback.onCreateSceneFinished(scene);
 	}
 
 	@Override
 	public void onPopulateScene(Scene pScene,
-			OnPopulateSceneCallback pOnPopulateSceneCallback)
+			IGameInterface.OnPopulateSceneCallback pOnPopulateSceneCallback)
 			throws IOException
 	{			
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
+	}
+	
+	public void createGlowRectangle()
+	{	
+        
+        Rectangle rec = new Rectangle(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2, CAMERA_WIDTH, CAMERA_HEIGHT, getVertexBufferObjectManager());
+        rec.setColor(Color.BLUE);
+        rec.setShaderProgram(mGlowShader);
+        rec.setZIndex(11);
+        rec.setBlendFunction(1,1);
+        scene.attachChild(rec);
+       
 	}
 
 	@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent)
 	{		
-		GlowShader mGlowShader = new GlowShader().getInstance(); 
 		float ratioX = mCamera.getSurfaceWidth()/mCamera.getWidth();
         float ratioY = mCamera.getSurfaceHeight()/mCamera.getHeight();
-        mGlowShader.setCentre(100.0f * ratioX, 100.0f * ratioY);
-        mGlowShader.setRadius(30.0f);
-        
-        Rectangle rec = new Rectangle(WIDTH / 2, HEIGHT / 2, 400, 400, getVertexBufferObjectManager());
-        rec.setColor(Color.TRANSPARENT);
-        rec.setShaderProgram(mGlowShader);
-        rec.setZIndex(11);
-        rec.setBlendFunction(1,1);
-        scene.attachChild(rec);
-
+        float x = pSceneTouchEvent.getX();
+        float y = pSceneTouchEvent.getY();
+		mGlowShader.setCentre(x * ratioX, y * ratioY);
+	    mGlowShader.setRadius(40.0f);
 		return false;
 	}
 	
