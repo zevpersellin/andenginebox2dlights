a45679e40d77c0cd23efe40a1f78ba98d85599fd
zevpersellin <zevpersellin@gmail.com>
2015-03-14 17:30:27 -0600

    Update GlowShaderTestActivity.java

--- src/com/andengine/lights/GlowShaderTestActivity.java
+++ src/com/andengine/lights/GlowShaderTestActivity.java
@@ -11,25 +11,29 @@ import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.shader.constants.ShaderProgramConstants;
+import org.andengine.opengl.shader.source.IShaderSource;
+import org.andengine.opengl.shader.source.StringShaderSource;
 import org.andengine.ui.IGameInterface;
 import org.andengine.ui.activity.BaseGameActivity;
-import org.andengine.util.adt.color.Color;
 
-import com.andengine.lights.shader.GlowShader;
-public class GlowShaderTestActivity extends BaseGameActivity implements IOnSceneTouchListener{
+import com.andengine.lights.shader.LightShader;
+
+public class LightsTestActivity extends BaseGameActivity implements IOnSceneTouchListener{
 	
-	public static final int CAMERA_WIDTH = 1080;
-	public static final int CAMERA_HEIGHT = 720;
+	public static final int WIDTH = 720;
+	public static final int HEIGHT = 480;
 	
 	private Camera mCamera;
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		this.mCamera = new Camera(0, 0, WIDTH, HEIGHT);
 		
-		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
+		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(WIDTH, HEIGHT), this.mCamera);
 
 		return engineOptions;
 	}
@@ -41,12 +45,11 @@ public class GlowShaderTestActivity extends BaseGameActivity implements IOnScene
         	
 			
 	}
-	GlowShader mGlowShader;
+
 	@Override
 	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback)	throws IOException {
 	
-		mGlowShader = new GlowShader().getInstance();
-		this.getShaderProgramManager().loadShaderProgram(mGlowShader);
+
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 	 Scene scene;
@@ -55,9 +58,10 @@ public class GlowShaderTestActivity extends BaseGameActivity implements IOnScene
 			throws IOException
 	{       
 			this.mEngine.registerUpdateHandler(new FPSLogger());
-	        scene = new Scene();	        	
+	        scene = new Scene();	        
+	        scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));	        
+	        getEngine().registerUpdateHandler(new FPSLogger());
 	        scene.setOnSceneTouchListener(this);
-	        createGlowRectangle();
 	        pOnCreateSceneCallback.onCreateSceneFinished(scene);
 	}
 
@@ -68,29 +72,44 @@ public class GlowShaderTestActivity extends BaseGameActivity implements IOnScene
 	{			
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
-	
-	public void createGlowRectangle()
-	{	
-        
-        Rectangle rec = new Rectangle(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2, CAMERA_WIDTH, CAMERA_HEIGHT, getVertexBufferObjectManager());
-        rec.setColor(Color.TRANSPARENT);
-        rec.setShaderProgram(mGlowShader);
-        rec.setZIndex(11);
-        rec.setBlendFunction(1,1);
-        scene.attachChild(rec);
-       
-	}
 
 	@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent)
 	{		
-		float ratioX = mCamera.getSurfaceWidth()/mCamera.getWidth();
-        float ratioY = mCamera.getSurfaceHeight()/mCamera.getHeight();
-        float x = pSceneTouchEvent.getX();
-        float y = pSceneTouchEvent.getY();
-		mGlowShader.setCentre(x * ratioX, y * ratioY);
-	    mGlowShader.setRadius(40.0f);
+		Rectangle r = new Rectangle(100f, 100f, 300f, 100f, getVertexBufferObjectManager());
+		LightShader lightShader = LightShader.getInstance();
+		r.setShaderProgram(lightShader);
+		getShaderProgramManager().loadShaderProgram(lightShader);
+		
+		scene.attachChild(r);
+		// TODO Auto-generated method stub
 		return false;
 	}
+	
+//	This is just a basic vector shader that andengine does... Its a vector that 
+	/*
+	 * AndEngine sends to OpenGL a list of points in space that represent what you are drawing to screen. 
+	 * These are the vertices of your scene -- for example the 4 points on each corner of a Sprite.
+	 *  The vertex shader is executed early in the pipeline and is called once for each vertex.
+
+		The final goal of the vertex shader is to return a position where that vertex should be rendered on the screen.
+		 It can do other things, like calculate normals, but that's not all that useful in a 2d world.
+	 */
+	static IShaderSource vShader = new StringShaderSource(
+		    "uniform mat4 " + ShaderProgramConstants.UNIFORM_MODELVIEWPROJECTIONMATRIX + ";\n" +
+		    "attribute vec4 " + ShaderProgramConstants.ATTRIBUTE_POSITION + ";\n" +
+		    "void main()        \n" +
+		    "{  \n" +
+		    "   gl_Position = " + ShaderProgramConstants.UNIFORM_MODELVIEWPROJECTIONMATRIX + " * " + ShaderProgramConstants.ATTRIBUTE_POSITION + ";\n" +
+		    "}"
+		);
+	static IShaderSource fShader = new StringShaderSource(
+		    "precision mediump float;           \n" +
+		    "uniform vec4 theColor;             \n" +
+		    "void main()                                        \n" +
+		    "{                                                  \n" +
+		    "   gl_FragColor = theColor;        \n" +
+		    "}"
+		);
 	
 }
